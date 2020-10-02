90249e3886f6ce97ba7eba692552ba7e19ef9a0c
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-02 22:57:36 +0800

    >  compile NEMU 3019244233 why Linux why-VirtualBox 5.3.0-53-generic #47~18.04.1-Ubuntu SMP Thu May 7 13:10:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:57:36 up 10:31,  2 users,  load average: 1.16, 1.07, 0.86 3d8ac4215bba7f8a404938a16a1f753a81656a7e

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -148,7 +148,12 @@ static int cmd_x(char *args) {
         }
         printf("N is %d\n",N);//测试能否输出N
 
-		char* expression = args ;
+		char* expression = strtok(NULL, " ") ;
+		while( expression != NULL ) {
+      	printf( "%s\n", expression );
+    
+      	expression = strtok(NULL, " ");
+  		 }
         if(expr == NULL)
         {
                 printf("Lack of parameter!\n");
