75a0f38de12c959b0aa25384fee778a99e9cbc2b
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-04 21:28:23 +0800

    >  run 20192160000 why Linux why-VirtualBox 5.3.0-53-generic #47~18.04.1-Ubuntu SMP Thu May 7 13:10:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:28:23 up  4:33,  1 user,  load average: 1.82, 1.45, 1.11 1763c8c64cd0d1a813b7d289f060a199a10e8674

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -2,6 +2,7 @@
 #include "monitor/expr.h"
 #include "monitor/watchpoint.h"
 #include "nemu.h"
+#include "common.h"
 
 #include <stdlib.h>
 #include <readline/readline.h>
@@ -11,101 +12,242 @@ void cpu_exec(uint32_t);
 
 /* We use the `readline' library to provide more flexibility to read from stdin. */
 char* rl_gets() {
-	static char *line_read = NULL;
+        static char *line_read = NULL;
 
-	if (line_read) {
-		free(line_read);
-		line_read = NULL;
-	}
+        if (line_read) {
+                free(line_read);
+                line_read = NULL;
+        }
 
-	line_read = readline("(nemu) ");
+        line_read = readline("(nemu) ");
 
-	if (line_read && *line_read) {
-		add_history(line_read);
-	}
+        if (line_read && *line_read) {
+                add_history(line_read);//行编辑的常用功能 历史记录
+        }
 
-	return line_read;
-}
-
-static int cmd_c(char *args) {
-	cpu_exec(-1);
-	return 0;
-}
-
-static int cmd_q(char *args) {
-	return -1;
+        return line_read;
 }
 
 static int cmd_help(char *args);
+static int cmd_c(char *args);
+static int cmd_q(char *args);
+static int cmd_si(char *args);
+static int cmd_info(char *args);
+static int cmd_x(char *args);
+static int cmd_p(char *args);
+static int cmd_w(char *args);
+static int cmd_d(char *args);
 
 static struct {
-	char *name;
-	char *description;
-	int (*handler) (char *);
+        char *name;
+        char *description;
+        int (*handler) (char *);//handler执行该操作的函数
 } cmd_table [] = {
-	{ "help", "Display informations about all supported commands", cmd_help },
-	{ "c", "Continue the execution of the program", cmd_c },
-	{ "q", "Exit NEMU", cmd_q },
-
-	/* TODO: Add more commands */
+        { "help", "Display informations about all supported commands", cmd_help },
+        { "c", "Continue the execution of the program", cmd_c },
+        { "q", "Exit NEMU", cmd_q },
+        /* TODO: Add more commands */
+        {"si","Run N single steps",cmd_si},
+        { "info", "Print regs' or watchpoints' state", cmd_info },
+        { "x", "Scan the memory", cmd_x },
+        {"p","Evaluate a expression", cmd_p},
+        { "w", "Set a watchpoint", cmd_w },
+        { "d", "Delete a watchpoint", cmd_d }
 
 };
 
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
 
 static int cmd_help(char *args) {
-	/* extract the first argument */
-	char *arg = strtok(NULL, " ");
-	int i;
+        /* extract the first argument */
+        char *arg = strtok(NULL, " ");//第一次用参数，第二次用NULL
+        int i;
 
-	if(arg == NULL) {
-		/* no argument given */
-		for(i = 0; i < NR_CMD; i ++) {
-			printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-		}
-	}
-	else {
-		for(i = 0; i < NR_CMD; i ++) {
-			if(strcmp(arg, cmd_table[i].name) == 0) {
-				printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-				return 0;
-			}
-		}
-		printf("Unknown command '%s'\n", arg);
-	}
-	return 0;
+        if(arg == NULL) {
+                /* no argument given */
+                for(i = 0; i < NR_CMD; i ++) {
+                        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+                }
+                
+        }
+        else {
+                for(i = 0; i < NR_CMD; i ++) {
+                        if(strcmp(arg, cmd_table[i].name) == 0) {
+                                printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+                                return 0;
+                        }
+                }
+                printf("Unknown command '%s'\n", arg);
+        }
+        return 0;
 }
 
+static int cmd_c(char *args) {
+        cpu_exec(-1);
+        return 0;
+}
+
+static int cmd_q(char *args) {
+        return -1;
+}
+
+static int cmd_si(char *args){
+        // printf("have enter the cmd_si\n");
+        //当N没有给出时, 缺省为1
+        if(args == NULL|| !(args[0]>='0'&&args[0]<='9')){
+                args = "1";
+        }
+        int singleStepRunNum = atoi(args);//atoi字符转数字
+        // printf("%-10d\n",singleStepRunNum);
+        ///程序单步执行N条指令后暂停
+        int i;
+        for(i = 0; i<singleStepRunNum;i++)
+                cpu_exec(1);
+        
+        return 0;
+}
+
+static int cmd_info(char *args) {
+        char *subcmd[] = {"r","w"};
+        // int i;
+        char *arg = strtok(NULL, " ");
+        if (arg != NULL)
+        {
+                        if(strcmp(arg, subcmd[0]) == 0)//"r"打印寄存器状态
+                        {
+                                int j;
+                                for(j=0;j<8;j++)
+                                        printf("%s\t\t0x%08x\n",regsl[j],cpu.gpr[j]._32);
+                                for(j=0;j<8;j++) 
+                                        printf("%s\t\t0x%04x\n",regsw[j],cpu.gpr[j]._16);
+                                for(j=0;j<8;j++) 
+                                        printf("%s\t\t0x%02x\n",regsb[j],cpu.gpr[j%4]._8[j/4]);
+                                return 0;
+                        }
+                        else if(strcmp(arg, subcmd[1]) == 0) //'w'打印监视点信息
+                        {
+                                show_watchpoint();
+                                return 0;
+                        }
+                        else
+                        {
+                                printf("Unknown command '%s'\n", arg);
+                                return 0;
+                        }       
+        }
+        printf("Lack of parameter!\n");
+        return 0;
+}
+
+static int cmd_x(char *args) {
+        char *arg = strtok(NULL, " ");
+        if(arg == NULL)
+        {
+                printf("Lack of parameter!\n");
+                return 0;
+        }
+        int printNumber = atoi(arg);
+        if(printNumber == 0){
+                printf("Unknown command '%s'\n",arg);
+                return 0;  //N=0时无法输出
+        }
+        // printf("printNumber is %d\n",printNumber);//测试能否输出N
+        
+		// char* e = strtok(NULL, " ") ;
+		// char* expression = e;
+		// printf("expression is %s\n", expression);
+		// while( e != NULL ) {
+      	// 	printf( "e is %s\n", e );
+    
+      	// 	e = strtok(NULL, " ");
+		//     strcat( expression , e);
+		// 	printf("expression is %s\n", expression);
+
+  		//  }
+	char* expression = strtok(NULL, " ");
+        if(expression == NULL)
+        {
+                printf("Lack of parameter!\n");
+                return 0;
+        }
+         printf("expression is %s\n",expression);//测试能否输出expr
+
+	bool *success=false;
+	// char *str;
+	swaddr_t addr = expr(expression,success);
+        int i;
+        for( i = 0; i < printNumber; i++)
+        {
+                printf("0x%08x:\t0x%08x\n",addr,swaddr_read(addr,4));
+                addr = addr+4;
+        }
+        return 0;
+}
+
+static int cmd_p(char *args) {
+        bool *success = false;
+        if(args == NULL){
+                printf("Lack of parameter!\n");
+                return 0;
+        }
+        uint32_t computedResult = expr(args, success);
+        printf("0x%08x\n", computedResult);
+        return 0;
+}
+
+static int cmd_w(char *args){
+         if(args == NULL){
+                printf("Lack of parameter!\n");
+                return 0;
+        }
+        bool *success = false;
+        WP* newWatchpoint =  new_watchpoint();
+        strcpy(newWatchpoint->expression,args);
+        newWatchpoint->address = expr(args,success);
+        newWatchpoint->value = swaddr_read(newWatchpoint->address,4);
+        printf("Set watchpoint NO.%d on 0x%08x\n",newWatchpoint->NO,newWatchpoint->address);
+        return 0;
+}
+
+static int cmd_d(char *args){
+        if(args == NULL){
+                printf("Lack of parameter!\n");
+                return 0;
+        }
+        int watchpointNO = atoi(args);
+        free_watchpoint(watchpointNO);
+        return 0;
+}
 void ui_mainloop() {
-	while(1) {
-		char *str = rl_gets();
-		char *str_end = str + strlen(str);
+        while(1) {
+                char *str = rl_gets();
+                char *str_end = str + strlen(str);
 
-		/* extract the first token as the command */
-		char *cmd = strtok(str, " ");
-		if(cmd == NULL) { continue; }
+                /* extract the first token as the command */
+                char *cmd = strtok(str, " ");
+                if(cmd == NULL) { continue; }
 
-		/* treat the remaining string as the arguments,
-		 * which may need further parsing
-		 */
-		char *args = cmd + strlen(cmd) + 1;
-		if(args >= str_end) {
-			args = NULL;
-		}
+                /* treat the remaining string as the arguments,
+                 * which may need further parsing//分析
+                 */
+                char *args = cmd + strlen(cmd) + 1;
+                if(args >= str_end) {
+                        args = NULL;
+                }
 
 #ifdef HAS_DEVICE
-		extern void sdl_clear_event_queue(void);
-		sdl_clear_event_queue();
+                extern void sdl_clear_event_queue(void);
+                sdl_clear_event_queue();
 #endif
 
-		int i;
-		for(i = 0; i < NR_CMD; i ++) {
-			if(strcmp(cmd, cmd_table[i].name) == 0) {
-				if(cmd_table[i].handler(args) < 0) { return; }
-				break;
-			}
-		}
+                int i;
+                for(i = 0; i < NR_CMD; i ++) {
+                        if(strcmp(cmd, cmd_table[i].name) == 0) {
+                                if(cmd_table[i].handler(args) < 0) { return; }
+                                break;
+                        }
+                }
 
-		if(i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
-	}
+                if(i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+        }
 }
--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -1,6 +1,8 @@
 #include "monitor/watchpoint.h"
 #include "monitor/expr.h"
 
+#include<stdlib.h>
+
 #define NR_WP 32
 
 static WP wp_pool[NR_WP];
@@ -19,5 +21,98 @@ void init_wp_pool() {
 }
 
 /* TODO: Implement the functionality of watchpoint */
+WP* new_watchpoint(){
+	if(head == NULL){
+        head = free_;
+        free_=free_->next;
+        head->next = NULL;
+        return head;
+    }
+    else{
+        if(free_ == NULL) //静态链表中没有空闲的位置了
+            assert(0);
+        WP* temp = head;
+        while(temp->next!=NULL){
+            temp = temp->next;
+        }
+        temp->next = free_;
+        free_ = free_->next;
+        temp = temp->next;
+        temp->next = NULL;
+        return temp;
+    }
+}
 
+void free_watchpoint(int watchpointNO){
+	assert(watchpointNO >= 0);
+	 WP *toDeletePoint = head;
+	 WP *previousToDeletePoint = head;
+	 //找到结点位置必定需要N
+	 //而watchpointNO==0时，无法先求previous（watchpointNO-1）,再求delete。
+	 //但watchpointNO==0是特殊位置可以并到后面的指针变动中
+	 //当watchpointNO>0时，先求previous（watchpointNO-1）,再求delete
+	 if(watchpointNO > 0){
+		//找到被删结点的前一个结点
+		while(previousToDeletePoint != NULL){
+        	if(previousToDeletePoint->NO == watchpointNO-1){
+            	break;
+			}
+        	previousToDeletePoint = previousToDeletePoint->next;
+    	}
+		//找到需要删除的结点
+		if(previousToDeletePoint->next != NULL)
+			toDeletePoint = previousToDeletePoint->next;
+		else
+		{
+			printf("Wrong position!\n");
+			return ;
+		}
+	}
+    if(head == toDeletePoint)//N == 0的情况
+        head = head->next;
+    else{
+		previousToDeletePoint->next = toDeletePoint->next;
+    }
+    toDeletePoint->next = free_;
+    free_ = toDeletePoint;
+    printf("Free watchpoint %d\n", watchpointNO);
 
+}
+
+void show_watchpoint(){
+	
+    WP *temp = head;
+	if(temp != NULL)
+		printf("Number\t\tValue\t\tAddress\t\tExpression\n");
+	else
+		printf("There is no watchpoints\n");
+	
+    while(temp!=NULL)
+    {
+        printf("%d\t\t0x%08x\t0x%08x\t%s\n",temp->NO, temp->value, temp->address,temp->expression);
+        temp = temp->next;
+    }
+}
+
+bool check_watchpoint(){
+	//有监视点变动，则返回true
+	WP *temp = head;
+    bool change = false;
+    while(temp!=NULL)
+    {	//检查每一个监视点
+
+        bool *success = false;
+        uint32_t newAddress = expr(temp->expression,success); 
+        if(newAddress != temp->address)
+        {
+            printf("Watchpoint %d: %s\n",temp->NO,temp->expression);
+            printf("Old value = 0x%08x\nNew value = 0x%08x\n",temp->address,newAddress);
+            temp->address = newAddress;//赋新值
+            change = true; 
+        }
+        temp = temp->next;
+    }
+    if(change)
+        return true;
+    return false;
+}
