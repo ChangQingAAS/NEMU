a4e69b4aa12381ff2ba81f37790114f5129de1ae
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-18 19:27:07 +0800

    >  run 20192160000 why Linux why-VirtualBox 5.3.0-53-generic #47~18.04.1-Ubuntu SMP Thu May 7 13:10:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:27:07 up  5:29,  1 user,  load average: 0.89, 0.89, 0.90 c9bd6e542f8dbdc867e21125dbd0185cac95fb96

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -12,20 +12,20 @@ void cpu_exec(uint32_t);
 
 /* We use the `readline' library to provide more flexibility to read from stdin. */
 char* rl_gets() {
-	static char *line_read = NULL;
+        static char *line_read = NULL;
 
         if (line_read) {
                 free(line_read);
                 line_read = NULL;
         }
 
-	line_read = readline("(nemu) ");
+        line_read = readline("(nemu) ");
 
-	if (line_read && *line_read) {
-		add_history(line_read);
-	}
+        if (line_read && *line_read) {
+                add_history(line_read);//行编辑的常用功能 历史记录
+        }
 
-	return line_read;
+        return line_read;
 }
 
 static int cmd_help(char *args);
@@ -39,23 +39,23 @@ static int cmd_w(char *args);
 static int cmd_d(char *args);
 
 static struct {
-	char *name;
-	char *description;
-	int (*handler) (char *);
+        char *name;
+        char *description;
+        int (*handler) (char *);//handler执行该操作的函数
 } cmd_table [] = {
-
-	/* TODO: Add more commands */
         { "help", "Display informations about all supported commands", cmd_help },
         { "c", "Continue the execution of the program", cmd_c },
         { "q", "Exit NEMU", cmd_q },
         /* TODO: Add more commands */
         {"si","Run N single steps",cmd_si},
         { "info", "Print regs' or watchpoints' state", cmd_info },
-        { "x", "Scan the mem", cmd_x },
-        { "p", "Expression evaluation", cmd_p },
-        { "w", "Set watchpoint", cmd_w },
-        { "d", "Delete watchpoint", cmd_d }
+        { "x", "Scan the memory", cmd_x },
+        {"p","Evaluate a expression", cmd_p},
+        { "w", "Set a watchpoint", cmd_w },
+        { "d", "Delete a watchpoint", cmd_d }
+
 };
+
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
 
 static int cmd_help(char *args) {
@@ -220,25 +220,25 @@ static int cmd_d(char *args){
         return 0;
 }
 void ui_mainloop() {
-	while(1) {
-		char *str = rl_gets();
-		char *str_end = str + strlen(str);
+        while(1) {
+                char *str = rl_gets();
+                char *str_end = str + strlen(str);
 
-		/* extract the first token as the command */
-		char *cmd = strtok(str, " ");
-		if(cmd == NULL) { continue; }
+                /* extract the first token as the command */
+                char *cmd = strtok(str, " ");
+                if(cmd == NULL) { continue; }
 
-		/* treat the remaining string as the arguments,
-		 * which may need further parsing
-		 */
-		char *args = cmd + strlen(cmd) + 1;
-		if(args >= str_end) {
-			args = NULL;
-		}
+                /* treat the remaining string as the arguments,
+                 * which may need further parsing//分析
+                 */
+                char *args = cmd + strlen(cmd) + 1;
+                if(args >= str_end) {
+                        args = NULL;
+                }
 
 #ifdef HAS_DEVICE
-		extern void sdl_clear_event_queue(void);
-		sdl_clear_event_queue();
+                extern void sdl_clear_event_queue(void);
+                sdl_clear_event_queue();
 #endif
 
                 int i;
