b0190daf764b9380c0efc0aee383b83ea90a4984
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-21 21:37:54 +0800

    >  compile NEMU 3019244233 why Linux why-VirtualBox 5.3.0-53-generic #47~18.04.1-Ubuntu SMP Thu May 7 13:10:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:37:54 up  3:34,  1 user,  load average: 0.58, 0.53, 0.52 a5f85e46eee59af4ee16967eaeaf841dd5d6d4d5

--- nemu/src/cpu/exec/ctrl-trans/je-template.h
+++ nemu/src/cpu/exec/ctrl-trans/je-template.h
@@ -3,9 +3,8 @@
 #define instr je
 
 static void do_execute () {
-	// DATA_TYPE_S imm = op_src -> val;
-    // print_asm("je\t%x", cpu.eip + 1 + DATA_BYTE + imm);
-    if (cpu.ZF == 1) cpu.eip += -5;
+    print_asm("je\t%x", cpu.eip + 1 + DATA_BYTE + op_src -> val);
+    if (cpu.ZF == 1) cpu.eip += op_src -> val;
 }
 
 make_instr_helper(i)
