7c45754d51f87a659903b8eba7db7790825f45f2
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-21 19:55:06 +0800

    >  compile NEMU 3019244233 why Linux why-VirtualBox 5.3.0-53-generic #47~18.04.1-Ubuntu SMP Thu May 7 13:10:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:55:06 up  1:52,  1 user,  load average: 0.68, 0.50, 0.45 fa02cace12b441468c4c838b0669ccf870d9292

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -3,20 +3,19 @@
 
 #define instr call
 
-static void do_execute() {
-         // push(ip)
-         cpu.eip += op_src->val;
-}
-make_instr_helper(i)
+// static void do_execute() {
+//          // push(ip)
+//          cpu.eip += op_src->val;
+// }
 
-// make_helper(concat(call_i_, SUFFIX)) {
-// 	int len = concat(decode_i_, SUFFIX)(cpu.eip + 1);
-//     reg_l(R_ESP) -= DATA_BYTE;
-//     swaddr_write(reg_l(R_ESP), 4, cpu.eip + len + 1);
-//     DATA_TYPE_S imm = op_src -> val;
-//     print_asm("call\t%x",cpu.eip + 1 + len + imm);
-//     cpu.eip += imm;
-//     return len + 1;
-// } 
+make_helper(concat(call_i_, SUFFIX)) {
+	int len = concat(decode_i_, SUFFIX)(cpu.eip + 1);
+    reg_l(R_ESP) -= DATA_BYTE;
+    swaddr_write(reg_l(R_ESP), 4, cpu.eip + len + 1);
+    DATA_TYPE_S imm = op_src -> val;
+    print_asm("call\t%x",cpu.eip + 1 + len + imm);
+    cpu.eip += imm;
+    return len + 1;
+} 
 
 #include "cpu/exec/template-end.h"
